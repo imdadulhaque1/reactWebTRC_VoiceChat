de4ae2969eef95752909879a4afdd433d2e91441
imdadulhaque1 <imdadulhaqueimdad820@gmail.com>
2021-12-13 15:44:01 +0600

    Create and customized Common Card.js file

--- react_voicechat/src/Components/Card/Card.js
+++ react_voicechat/src/Components/Card/Card.js
@@ -1,1 +1,16 @@
+import React from 'react';
+import styles from './Card.module.css';
+import LogoImdad from '../../assets/images/Imdad.png';
+function Card() {
+    return (
+        <div className={styles.card}>
+                <div className={styles.headingWrapper}>
+                    <img src={LogoImdad} height={40} width={70} alt="Imdadul Haque"/>
+                    <h2 className={styles.heading}>Welcome to chatting room!</h2>
+                </div>
 
+            </div>
+    )
+}
+
+export default Card;
