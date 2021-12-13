e7a791a15b74536d21814986a99f34d89e390c84
imdadulhaque1 <imdadulhaqueimdad820@gmail.com>
2021-12-13 15:43:08 +0600

    Customized Home.js file

--- react_voicechat/src/Components/Home/Home.js
+++ react_voicechat/src/Components/Home/Home.js
@@ -1,10 +1,33 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom';
+import styles from './Home.module.css';
+import LogoImdad from '../../assets/images/Imdad.png';
+import backImg from '../../assets/images/back.png';
 function Home() {
     return (
-        <div>
-            <h1> App page!</h1>
+        <div className={styles.cardWrapper}>
+            <div className={styles.card}>
+                <div className={styles.headingWrapper}>
+                    <img src={LogoImdad} height={40} width={70} alt="Imdadul Haque"/>
+                    <h2 className={styles.heading}>Welcome to chatting room!</h2>
+                </div>
+                <p className={styles.pText}>
+                    Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.
+                </p>
+                <div>
+                    <button>
+                        <span>Get your username</span>
+                        <img src={backImg} alt="Back Icon" height={10} width={20} />
+                    </button>
+                </div>
+                <div>
+                    <span>have an invite text?</span>
+                    <Link to='/login'>Sign in</Link>
+                </div>
+
+            </div>
         </div>
+        
     )
 }
 
